1f14dc87e26cbb49a3c5361685d22f0a7aced4e9
yousefmaheralnajar <yousef.maher.alnajar@gmail.com>
2026-04-03 17:57:24 +0200

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
